d691ad290ca21d09bf38d31bab69b471b0e81487
Amandeep112 <a.amandeep@tru.agency>
2023-09-11 17:49:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  // eslint-disable-next-line no-undef
+  useEffect(() => {
+    var webURL;
+    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      webURL = `itms-apps://apps.apple.com/ca/app/the-beer-store/id1623374239?platform=iphone`;
+    } else {
+      webURL = `market://details?id=com.beerstore`;
+    }
+    window.open(webURL, "_blank");
+    window.location.href = webURL;
+    var isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    var deepLinkURL = `thebeerstore://`;
+    if (isMobile) {
+      window.location.href = deepLinkURL;
+      setTimeout(function () {
+        // If the app does not open, fall back to the browser
+        window.open(webURL, "_blank");
+        window.location.href = webURL;
+      }, 1000);
+    } else {
+      // Open in the browser on non-mobile devices
+      window.open("https://www.thebeerstore.ca/", "_blank");
+      window.location.href = "https://www.thebeerstore.ca/";
+    }
+  }, []);
+
+  return <div className="App"></div>;
+}
+
+export default App;
