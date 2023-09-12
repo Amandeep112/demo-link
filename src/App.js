fe9842d064b1676dcde2bf242a4e008e418c8aa6
Amandeep112 <103934091+Amandeep112@users.noreply.github.com>
2023-09-12 12:28:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       }, 1000);
     } else {
       // Open in the browser on non-mobile devices
-      window.open("https://www.thebeerstore.ca/", "_blank");
+      window.open("https://www.thebeerstore.ca/", "_blank").focus();
       window.location.href = "https://www.thebeerstore.ca/";
     }
   }, []);
