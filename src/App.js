39cb239c3b95810853de0b6cc4dca7d67294460c
Amandeep112 <103934091+Amandeep112@users.noreply.github.com>
2023-09-12 11:36:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,41 +4,26 @@ import { useEffect } from "react";
 
 function App() {
   // eslint-disable-next-line no-undef
-   useEffect(() => {
-   
-        // var webURL;
-        // if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-        //   webURL =
-        //     "https://itunes.apple.com/us/app/the-beer-store/id1623374239?platform=iphone";
-        // } else {
-        //   webURL = `market://details?id=com.beerstore`;
-        // }
-        // window.open(webURL, "_blank");
-        // window.location.href = webURL;
-        // var isMobile =
-        //   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        //     navigator.userAgent
-        //   ) && !window.MSStream;
-
-        // var deepLinkURL = `thebeerstore://`;
-        if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-          window.location.href = `thebeerstore://`;
-
-          setTimeout(function () {
-            // Link to the App Store should go here -- only fires if deep link fails
-            window.open('https://itunes.apple.com/us/app/the-beer-store/id1623374239?platform=iphone', "_blank");
-            window.location.href = 'https://itunes.apple.com/us/app/the-beer-store/id1623374239?platform=iphone';
-          }, 500);
-        } else {
-          // Open in the browser on non-mobile devices
-          window.open(
-            "https://itunes.apple.com/us/app/the-beer-store/id1623374239?platform=iphone",
-            "_blank"
-          );
-          window.location.href =
-            "https://itunes.apple.com/us/app/the-beer-store/id1623374239?platform=iphone";
-        }
-      
+  useEffect(() => {
+    var fallbackLink =
+      "https://itunes.apple.com/ca/app/the-beer-store/id1623374239?platform=iphone" +
+      window.location.search +
+      window.location.hash;
+    var isiOS =
+        navigator.userAgent.match("iPad") ||
+        navigator.userAgent.match("iPhone") ||
+        navigator.userAgent.match("iPod"),
+      isAndroid = navigator.userAgent.match("Android");
+    if (isiOS || isAndroid) {
+      document.getElementById("loader").src =
+        "thebeerstore://" + window.location.search + window.location.hash;
+      fallbackLink = isAndroid
+        ? "market://details?id=com.beerstore"
+        : "https://itunes.apple.com/ca/app/the-beer-store/id1623374239?platform=iphone";
+    }
+    window.setTimeout(function () {
+      window.location.replace(fallbackLink);
+    }, 1);
   }, []);
 
   return <div className="App">
