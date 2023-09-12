976779feb5ee95efbdd84e987fa10a5925b77f99
Amandeep112 <103934091+Amandeep112@users.noreply.github.com>
2023-09-12 13:22:47 +0530

    Update App

--- src/App.js
+++ src/App.js
@@ -2,46 +2,19 @@ import React, { useEffect } from 'react';
 import branch from 'branch-sdk';
 
 function App() {
-  useEffect(() => {
-    async function checkAndOpenApp() {
-      // Initialize the Branch SDK with your Branch.io key
-      branch.init('YOUR_BRANCH_KEY', async (err, data) => {
-        if (err) {
-          console.error(err);
-          redirectToWebsite();
-          return;
-        }
+ useEffect(() => {
+    // Initialize the Branch SDK
+    branch.isAppInstalled("1623374239", function (err, isInstalled) {
+      if (err) throw err;
 
-        // Generate a deep link URL for your app
-        const deepLinkURL = 'thebeerstore://';
-
-        try {
-          // Check if the app is installed
-          const isAppInstalled = await branch.isAppInstalled();
-          
-          if (isAppInstalled) {
-            // App is installed, open the app
-            window.location.href = deepLinkURL;
-          } else {
-            // App is not installed, redirect to the website
-            redirectToWebsite();
-          }
-        } catch (error) {
-          console.error(error);
-          // An error occurred while checking app installation status, redirect to the website
-          // redirectToWebsite();
-          window.location.href = 'https://www.thebeerstore.ca/';
-        }
-      });
-
-      // Function to redirect to the website
-      function redirectToWebsite() {
-        window.location.href = 'https://itunes.apple.com/ca/app/the-beer-store/id1623374239?platform=iphone';
+      if (isInstalled) {
+        // The app is installed, you can take some action
+        console.log("App is installed");
+      } else {
+        // The app is not installed, you can redirect the user to the App Store or Play Store
+        console.log("App is not installed");
       }
-    }
-
-    // Call the checkAndOpenApp function when the component mounts
-    checkAndOpenApp();
+    });
   }, []);
 
   return (
