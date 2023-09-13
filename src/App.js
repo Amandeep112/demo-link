25c670225091d6dc7b8b505f35e765ad9e9bf4ff
Amandeep112 <103934091+Amandeep112@users.noreply.github.com>
2023-09-13 18:09:26 +0530

    Update App.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useEffect } from "react";
 
 function App() {
@@ -13,27 +11,16 @@ function App() {
         navigator.userAgent.match("iPhone") ||
         navigator.userAgent.match("iPod"),
       isAndroid = navigator.userAgent.match("Android");
-    var appleExpression = /Apple/i.test(navigator.userAgent);
-    var safariExpression = /Safari/i.test(navigator.userAgent);
-    var ua = navigator.userAgent.toLowerCase();
-    if (ua.indexOf("safari") != -1) {
-      if (ua.indexOf("chrome") > -1) {
-        alert("1"); // Chrome
-      } else {
-        alert("2"); // Safari
-      }
-    }
     try {
       if (
         navigator.userAgent.indexOf("Safari") !== -1 &&
         navigator.userAgent.indexOf("Chrome") === -1
       ) {
-        window.open("thebeerstore://", "_blank");
-        window.location.href = "thebeerstore://";
-        window.setTimeout(function () {
-          window.location.replace("thebeerstore://");
-          window.alert("fallback detected");
-        }, 1000);
+        window.open(
+          "https://tbsecomd.wpengine.com/deep-linking/bs-deep-linking.html",
+          "_blank"
+        );
+
         window.alert("enter IN safari");
       } else {
         if (isiOS || isAndroid) {
